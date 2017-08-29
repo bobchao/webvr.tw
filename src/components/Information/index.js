0911a7f4e24dff04444c1799ecca47ab7efcac9f
qas612820704@gmail.com <qas612820704@gmail.com>
2017-08-29 17:12:59 +0800

    Update footer contribute link more obvious

--- src/components/Information/index.js
+++ src/components/Information/index.js
@@ -24,7 +24,7 @@ export default () => (
   <section className="block text-container">
     <div className="md-grid">
       <div className="md-cell md-cell--12">
-        <p>大家好! 歡迎光臨<del>漢堡王</del><sapn className="second-color">WebVR.tw</sapn>, 這裡有一些有用的連結給大家參考</p>
+        <p>大家好! 歡迎光臨 <sapn className="second-color">WebVR.tw</sapn>, 這裡有一些有用的連結給大家參考</p>
       </div>
       { cardsData.map((data) => (
         <Card key={data.title} className="md-cell md-cell--6" {...data} />
